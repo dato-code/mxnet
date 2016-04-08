579da15963683ca58557657ae291a63cc9155807
Jay Gu <jgu@dato.com>
2016-04-07 23:46:38 -0700

    fix build script; improve message on import

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,6 @@
 # Environment variables:
 # CUDA_PATH: set to build with cuda at particular location
 # TEST_GPU: set to run gpu unittest
-# SKIP_TEST: set to skip unittest
 # BUILD_NUMBER: the build number of the final artifact
 ##########
 
@@ -122,22 +121,20 @@ rm -rf ${TARGET_DIR}/*.tar.gz
 # Cleanup previous build ##
 clean_target_dir
 
-## Build ##
+## Standard build ##
+build
+unittest
+LIB_NAME='libmxnet'
+copy_artifact
+
+## GPU build ##
 if [[ ! -z "$CUDA_PATH" ]]; then
   build_with_cuda
+  if [[ ! -z "$TEST_GPU" ]]; then
+    unittest_with_cuda
+  fi
   LIB_NAME='libmxnet.cuda'
-else
-  build
-  LIB_NAME='libmxnet'
+  copy_artifact
 fi
 
-## Test ##
-if [[ ! -z "$TEST_GPU" ]]; then
-  unittest_with_cuda
-elif [[ -z "$SKIP_TEST" ]]; then
-  unittest
-fi
-
-## Copy artifacts and package into tar ball ## 
-copy_artifact
 package
