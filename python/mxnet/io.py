Added test, fixed indentation by replacing tabs with spaces, fixed bug caused by broadcasting

python/mxnet/io.py
@@ -655,7 +655,7 @@ class SFrameImageIter(SFrameIter):
     scale : float, optional
         multiply each pixel value by the scale (this operation is performed after mean subtraction)
     random_flip : bool, optional 
-	Randomly flip horizontally on the fly, useful to augment data for training neural network. 
+        Randomly flip horizontally on the fly, useful to augment data for training neural network. 
     **kwargs :
         placeholder for new parameters
 
@@ -680,7 +680,7 @@ class SFrameImageIter(SFrameIter):
                  mean_b=0.0,
                  mean_nd=None,
                  scale=1.0,
-		 random_flip=False,
+                 random_flip=False,
                  **kwargs):
         super(SFrameImageIter, self).__init__(sframe, data_field, label_field, batch_size,
                                               data_name, label_name)
@@ -707,8 +707,8 @@ class SFrameImageIter(SFrameIter):
         # Rescale parameters
         self._scale = scale
 
-	#Augmentation parameters
-	self._random_flip = random_flip
+        #Augmentation parameters
+        self._random_flip = random_flip
     def _type_check(self, sframe, data_field, label_field):
         if label_field is not None:
             label_column_type = sframe[label_field].dtype()
@@ -735,9 +735,9 @@ class SFrameImageIter(SFrameIter):
         ret = super(self.__class__, self).iter_next()
         # Postprocess: normalize by mean, scale, ...
         self.data_ndarray = (self.data_ndarray - self._rgb_mask) * self._scale
-	# random flip
-	if self._random_flip:
-	    self.data_nd_array = self.data_nd_array[:,:,::(np.random.randint(2) - 0.5) * 2]
+        # random flip
+        if self._random_flip:
+            self.data_ndarray = array(self.data_ndarray.asnumpy()[:,:,:,::(np.random.randint(2)- 0.5) * 2])
         return ret
 
 

tests/python/unittest/test_sframe_image_iter.py
@@ -21,7 +21,7 @@ except:
 @unittest.skipIf(__has_sframe__ is False and __has_graphlab__ is False, 'graphlab or sframe not found')
 class SFrameImageIteratorBaseTest(unittest.TestCase):
     def setUp(self):
-        (w, h, c, n) = (2, 4, 3, 10)
+        (w, h, c, n) = (2, 4, 3, 100)
         self.images = [np.random.randint(256, size=(h,w,c)) for i in range(n)]
         self.data = gl.SFrame({'arr': [array.array('d', x.flatten()) for x in self.images],
                               'y': np.random.randint(2, size=n)})
@@ -97,3 +97,36 @@ class SFrameImageIteratorBaseTest(unittest.TestCase):
             x[:,:,:,2] += mean_b
             data_actual.extend(x.flatten())
         np.testing.assert_almost_equal(data_actual, self.data_expected)
+
+    def test_random_flip(self): 
+        it = mxnet.io.SFrameImageIter(self.data, data_field=self.data_field,
+                                 label_field=self.label_field,
+                                 batch_size=1,
+                                 random_flip=True)
+        data_actual = []
+        data_actual_flipped = []
+        for d in it:
+            # reorder from (batch, channel, height, width) to (batch, height, width, channel)
+            x = d.data[0].asnumpy()
+            x = np.swapaxes(x, 1, 3)
+            x = np.swapaxes(x, 1, 2)
+            data_actual.append(x.flatten())
+
+            x_flipped = d.data[0].asnumpy()[:,:,:,::-1]
+            x_flipped = np.swapaxes(x_flipped, 1, 3)
+            x_flipped = np.swapaxes(x_flipped, 1, 2)
+            data_actual_flipped.append(x_flipped.flatten())
+        
+        num_flipped = 0
+        for expected, actual, flipped in zip(list(self.data['arr']), data_actual, data_actual_flipped):
+            if np.allclose(expected,actual):
+                pass
+            elif np.allclose(expected, flipped):
+                num_flipped = num_flipped + 1
+            else:
+                assert self.assertTrue(False)
+
+        flipped_ratio = float(num_flipped)/self.data_size
+
+        #Check that roughly half images are flipped
+        self.assertAlmostEqual(flipped_ratio, 0.5, delta=0.2)